cf767693e6c2de950a0fa45b214927f3296647c4
Fabian Müller <fabianmueller100295@gmail.com>
2023-09-26 14:26:16 +0200

    developing the principles

--- src/BioprocessModels.jl
+++ src/BioprocessModels.jl
@@ -1,6 +1,7 @@
 module BioprocessModels
-
+export reaction_system, compose_kinetics, flow, gasflow, tank, component, kinetic, monod, blackman, teissier, moser, haldane, monod_haldane
 using ModelingToolkit
+using Symbolics
 
 # Defining the independent variable time t and the differential operator D
 @parameters t
@@ -22,10 +23,12 @@ function flow(; name)
     ]
     ODESystem(eqs, t, [F_m,F], [ρ], name = name)
 end
+# @named fl = flow()
+
 
 function gasflow(; name)
     @constants (
-        V_nM = 22.4, [description="molar volume [L/mol]"]
+        V_nM = 22.414, [description="molar volume [L/mol]"]
     )
     @variables (
         F(t), [description="gasflow rate [L/h]"],
@@ -34,9 +37,9 @@ function gasflow(; name)
     eqs = [
         F_n ~ F / V_nM,
     ]
-    ODESystem(eqs, t, [Q_L,F_L], [ρ], name = name)
+    ODESystem(eqs, t, name = name)
 end
-
+# @named fng = gasflow()
 """
 tank(; name, nrInFlows, nrOutFlows)
 
@@ -57,17 +60,18 @@ function tank(; name, nrInFlows=2, nrOutFlows=1, nrGasFlows=1)
     @variables (
         V_L(t), [description="liquid volume [L]"],
         )
-    inFlows = [flow(name="inflow$i") for i in 1:nrInFlows]
-    outFlows = [flow(name="outflow$i") for i in 1:nrOutFlows]
-    gasFlows = @variables(F_G(t), :gasflow, 1:nrGasFlows)
+    inFlows = [flow(name=Symbol("inflow$i")) for i in 1:nrInFlows]
+    outFlows = [flow(name=Symbol("outflow$i")) for i in 1:nrOutFlows]
+    gasFlows = [gasflow(name=Symbol("gasflow$i")) for i in 1:nrGasFlows]
     eqs = [
-        D(V_L) ~ sum(F_L_in) - sum(F_L_out),
-
-    ]
-    compose(ODESystem(eqs, t, [V_L], [ρ], name = name), inFlows, outFlows)
+        D(V_L) ~ sum([inFlow.F for inFlow in inFlows]) - sum([outFlow.F for outFlow in outFlows]),
+        ]
+    compose(ODESystem(eqs, t, [V_L], [ρ], name = name), inFlows..., outFlows...)
 end
+# @named F10 = tank()
 
-function component(; name, catalyst::ModelingToolkit.AbstractODESystem=nothing)
+
+function component(; name, catalyst=false)
     @parameters (
         M, [description="molar mass [g/mol]"]
     )
@@ -79,16 +83,110 @@ function component(; name, catalyst::ModelingToolkit.AbstractODESystem=nothing)
         q(t), [description="specific reaction rate [g/g/h]"],
         )
     eqs = [
-        catalyst ? r ~ q * catalyst.m : r ~ q * m,
+        isa(catalyst,ModelingToolkit.AbstractODESystem) ? r ~ q * catalyst.m : r ~ q * m,
         r_n ~ r / M,
         n ~ m / M,
     ]
     ODESystem(eqs, t, [m,n,r,r_n,q], [M], name = name)
 end
 
-function monod(; name, K = 0.05, q_max = 1.1)
-    
+function reaction_system(componentsArray, reactionsArray; name)
+    qi = [reaction.r for reaction in reactionsArray]
+    q = [component.q for component in componentsArray]
+    @parameters Y
+    for (index_c, component) in enumerate(componentsArray)
+        if index_c > 1
+            Y = hcat(Y, [eval(Meta.parse("@parameters Y_" * String(component.name) * "_" * String(reaction.name)))[1] for reaction in reactionsArray])
+        else
+            Y = [eval(Meta.parse("@parameters Y_" * String(component.name) * "_" * String(reaction.name)))[1] for reaction in reactionsArray]
+        end
+    end
+    Y = Y'
+    @variables r(t), [description="Reaction rate"]
+    #@parameters Y[1:length(componentsArray), 1:length(reactionsArray)], [description="Yields [g/g]"]
+    compose(ODESystem(q .~ Y * qi, t, name = name), componentsArray..., reactionsArray...)
 end
+
+function compose_kinetics(; name, kin1::ModelingToolkit.AbstractODESystem, kin2::ModelingToolkit.AbstractODESystem)
+    @variables r̃(t), [description="Reaction rate"]
+    compose(ODESystem([r̃ ~ kin1.r̃ * kin2.r̃], name = name), kin1, kin2)
+end
+function monod_haldane(; name)
+    @variables r̃(t), [description="Reaction rate"]
+    @named mnd = monod()
+    @named hld = haldane()
+    compose(ODESystem([r̃ ~ mnd.r̃ * hld.r̃], name = name), mnd, hld)
+end
+"denormalize the kinetic expressions"
+function kinetic(kin::ModelingToolkit.AbstractODESystem; name, r_max=1)
+    @parameters r_max=r_max, [description="Maximal reaction rate"]
+    @variables r(t), [description="Reaction rate"]
+    @unpack r̃ = kin
+    extend(ODESystem([r ~ r_max * r̃], name = name), kin)
+end
+
+function monod(; name, K=0.05)
+    @parameters K=K, [description="Monod constant"]
+    @variables (
+        r̃(t), [description="Reaction rate"],
+        c(t), [description="Substrate concentration"]
+        )
+    ODESystem([r̃ ~ c / (K + c)], t, [r̃,c], [K], name = name)
+end
+# @named black = blackman(K=0.1)
+# @named qiS = kinetic(black, r_max = 1.2)
+
+function blackman(; name, K=0.05)
+    @parameters K=K, [description="Blackman constant"]
+    @variables (
+        r̃(t), [description="Reaction rate"],
+        c(t), [description="Substrate concentration"]
+        )
+    ODESystem([r̃ ~ min(1, K * c)], t, [r̃,c], [K], name = name)
+end
+
+function teissier(; name, K=0.05)
+    @parameters K=K, [description="Teissier constant"]
+    @variables (
+        r̃(t), [description="Reaction rate"],
+        c(t), [description="Substrate concentration"]
+        )
+    ODESystem([r̃ ~ 1 - exp(-K * c)], t, [r̃,c], [K], name = name)
+end
+
+function moser(; name,K=0.05,N=2)
+    @parameters (
+        K=K, [description="Moser constant"],
+        N, [description="Moser order"]
+    )
+    @variables (
+        r̃(t), [description="Reaction rate"],
+        c(t), [description="Substrate concentration"]
+        )
+    ODESystem([r̃ ~ c^N / (K^N + c^N)], t, [r̃,c], [K,N], name = name)
+end
+
+"haldane kinetics"
+function haldane(; name, K=0.05, K_i=0.05)
+    @parameters (
+        K=K, [description="Haldane constant"],
+        K_i=K_i, [description="Haldane inhibition constant"]
+    )
+    @variables (
+        r̃(t), [description="Reaction rate"],
+        c(t), [description="Substrate concentration"]
+        )
+    ODESystem([r̃ ~ c / (K + c * (1 + c / K_i))], t, [r̃,c], [K,K_i], name = name)
+end
+
+# macro species(name)
+#     component(name = name)
+# end
+
+#@species T1
+
+####### TESTS #######
+# First build the components vector 
 
 # X = component(name="X")
 # component.(name=namevector, catalyst = X)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,42 @@
+using Revise
 using BioprocessModels
+using ModelingToolkit
+using Latexify
+using Symbolics
 using Test
+
+@named R = tank(nrInFlows=1, nrOutFlows=1)
+
+# Define the components
+@named X = component()
+@named S = component(catalyst=X)
+# @named P = component(catalyst=X)
+# @named CO2 = component(catalyst=X)
+# @named O2 = component(catalyst=X)
+components = [X, S]#, P, CO2, O2]
+
+# Define the reactions and couple to components
+@named mnd = monod()
+@named hld = haldane()
+@named qiS = kinetic(mnd)
+# @named mndhld = monod_haldane()
+# @named qiP = kinetic(mndhld)
+# @named mndhld2 = compose_kinetics(kin1=mnd, kin2=hld)
+# @named qiP2 = kinetic(mndhld2)
+reactions = [qiS]
+
+# Define the reaction system
+@named react = reaction_system(components, reactions)
+
+# Compose 
+
+
+full_equations(react)
+parameters(react)
+equations(qiP)
+latexify(parameters(qiP2))
+@named components = compose(R, X, S, P, CO2, O2)
+
 
 @testset "BioprocessModels.jl" begin
     # Write your tests here.
